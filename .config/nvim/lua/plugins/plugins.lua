6c5d261404748a737482a03f9eac083374e2d49e
Dane Lipscombe <danelipscombe@gmail.com>
2025-11-11 11:45:18 +1100

    Setup lazyvim

---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,5 @@
+return {
+  { "mason-org/mason-lspconfig.nvim", enabled = not vim.env.IS_NIX },
+  { "mason-org/mason.nvim", enabled = not vim.env.IS_NIX },
+  { "alexghergh/nvim-tmux-navigation" },
+}
