d0dc4459692a89ac3c0819723937d7fb1324bc77
Dane Lipscombe <danelipscombe@gmail.com>
2025-11-18 11:31:34 +1100

    Add smart picker

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,11 +2,20 @@ return {
   "folke/snacks.nvim",
   keys = {
     {
+      "<leader>a",
+      function()
+        Snacks.picker.smart({
+          multi = { "buffers", "git_files" },
+        })
+      end,
+      desc = "Smart file picker",
+    },
+    {
       "<leader>fr",
       function()
         Snacks.picker.recent({ filter = { cwd = true } })
       end,
-      desc = "Recent",
+      desc = "Recent files",
     },
   },
 }
