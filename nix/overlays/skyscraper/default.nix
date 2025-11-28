2a93674dd5c4881027e5f05151ab94cc0b1e94db
Dane Lipscombe <danelipscombe@gmail.com>
2025-11-28 14:44:35 +1100

    skyscraper

--- nix/overlays/skyscraper/default.nix
+++ nix/overlays/skyscraper/default.nix
@@ -1,21 +1,61 @@
-{ stdenv, qt5, fetchFromGitHub, ... }:
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  qt5,
+  p7zip,
+  python3,
+  installShellFiles,
 
-stdenv.mkDerivation {
-  name = "skyscraper";
+  # Whether to compile with XDG support
+  # (See: https://gemba.github.io/skyscraper/XDG/)
+  enableXdg ? false,
+}:
 
-  buildInputs = [
-    qt5.qmake
-  ];
-
-  installPhase = ''
-    mkdir -p $out/bin
-    cp Skyscraper $out/bin/
-  '';
+stdenv.mkDerivation (finalAttrs: {
+  pname = "skyscraper";
+  version = "3.18.0";
 
   src = fetchFromGitHub {
+    owner = "Gemba";
     repo = "skyscraper";
-    owner = "muldjord";
-    rev = "f98480be6607608bfe07f83cab097bcfeff086c3";
-    sha256 = "CqGCUxvGuQ7JgkwhNUaEeFFgZPfyzdG1i8fVgkCOv0M=";
+    rev = "97d87bdaa137c26b43ae001a4b2abd54ec092749";
+    hash = "sha256-Gg84XRVWvTOVbCeT1ZacS5jY5K4vT0M7KaJftVtCPr4=";
   };
-}
+
+  strictDeps = true;
+
+  nativeBuildInputs = [
+    qt5.wrapQtAppsHook
+    qt5.qmake
+    installShellFiles
+  ];
+
+  buildInputs = [ python3 ];
+
+  postPatch = lib.optionalString enableXdg ''
+    substituteInPlace skyscraper.pro --replace-fail "#DEFINES+=XDG" "DEFINES+=XDG"
+  '';
+
+  postInstall = ''
+    installShellCompletion --cmd Skyscraper \
+      --bash supplementary/bash-completion/Skyscraper.bash
+  '';
+
+  preFixup = ''
+    qtWrapperArgs+=(--prefix PATH : ${lib.makeBinPath [ p7zip ]})
+    chmod +x $out/bin/*.py
+  '';
+
+  env.PREFIX = placeholder "out";
+
+  meta = {
+    description = "Powerful and versatile game data scraper written in Qt and C++";
+    homepage = "https://gemba.github.io/skyscraper/";
+    downloadPage = "https://github.com/Gemba/skyscraper/releases/tag/${finalAttrs.version}";
+    license = lib.licenses.gpl3Plus;
+    maintainers = with lib.maintainers; [ ashgoldofficial ];
+    mainProgram = "Skyscraper";
+    platforms = lib.platforms.linux;
+  };
+})
