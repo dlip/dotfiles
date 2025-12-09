725561e081ce13801b5165bca65c5509d2565b8a
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-09 13:48:41 +1100

    Sort groups

--- nix/pkgs/groups.nix
+++ nix/pkgs/groups.nix
@@ -1,33 +1,10 @@
 { pkgs, ... }:
+with pkgs;
 let
-  default = with pkgs; [
-    gh-markdown-preview
-    gopls
-    markdownlint-cli2
-    marksman
-    nil
-    nixfmt-rfc-style
-    prettier
-    pyright
-    ruff
-    rust-analyzer
-    shfmt
-    stylua
-    lua-language-server
-    tree-sitter
-    font-awesome
-    fontconfig
-    hachimarupop
-    hanazono
-    noto-fonts
-    noto-fonts-cjk-sans
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-    nerd-fonts.roboto-mono
-    nerd-fonts.sauce-code-pro
-    cargo
+  default = [
     bat
     bc
+    cargo
     curl
     delta
     dig
@@ -35,38 +12,62 @@ let
     entr
     fd
     findutils
+    font-awesome
+    fontconfig
     fzf
     gcc
     gh
+    gh-markdown-preview
     git
     gnugrep
     gnumake
+    gnused
+    gopls
     gotop
+    hachimarupop
+    hanazono
     htop
     iconv
     jq
     killall
     lazygit
     lsd
+    lua-language-server
+    markdownlint-cli2
+    marksman
     ncdu
     ncurses
     neofetch
-    nettools
     neovim
+    nerd-fonts.droid-sans-mono
+    nerd-fonts.fira-code
+    nerd-fonts.roboto-mono
+    nerd-fonts.sauce-code-pro
+    nettools
+    nil
+    nixfmt-rfc-style
     nmap
-    p7zip
-    ripgrep
-    gnused
+    noto-fonts
+    noto-fonts-cjk-sans
     openssh
-    speedread
+    p7zip
+    prettier
+    pyright
+    ripgrep
+    ruff
+    rust-analyzer
+    shfmt
     sops
+    speedread
     sqlite
     sshfs
     stack
     starship
+    stylua
     tcpdump
     tmux
     tree
+    tree-sitter
     unzip
     util-linux
     wget
@@ -76,240 +77,239 @@ let
     zoxide
     zsh
   ];
-  extra =
-    with pkgs;
-    default
-    ++ [
-      acpi
-      appimage-run
-      binutils
-      btop
-      # mimic
-      btop
-      evtest
-      hdparm
-      # fusee-launcher
-      iotop
-      iputils
-      kanata
-      # kdePackages.k3b # export QTWEBENGINE_DISABLE_SANDBOX=1; sudo -EH k3b
-      kmonad
-      krename
-      mecab
-      mkvtoolnix-cli
-      # nix-du
-      ns-usbloader
-      pinentry-curses
-      # python311Packages.adafruit-nrfutil
-      qmk
-      strace
-      tagainijisho
-      tiramisu
-      traceroute
-      usbutils
-      usbimager
-      ventoy
-      xdotool
-      xorg.xhost # add root to xsession: xhost si:localuser:root
-      archivemount
-      age
-      air
-      ansifilter
-      autoreconfHook
-      avrdude
-      patchelf
-      nix-index
-      stable.awscli2
-      blisp
-      bun
-      docker-buildx
-      cargo
-      cargo-wasi
-      cheat
-      clang-tools
-      delve
-      deno
-      dive
-      difftastic
-      docker-compose
-      eksctl
-      exiv2
-      ffmpeg
-      file
-      fluxcd
-      ghc
-      glow
-      gnupg
-      gnuplot
-      go
-      gotypist
-      graphviz
-      hottext
-      speedread
-      hexdino
-      imagemagick
-      jc
-      joshuto
-      jdk11
-      k9s
-      kaf
-      kind
-      kubectl
-      kubectx
-      kubernetes-helm
-      kubetail
-      lldb
-      lsof
-      (lua.withPackages (ps: with ps; [ luacheck ]))
-      manga-tui
-      massren
-      mdbook
-      mdl
-      mysql80
-      # musikcube
-      # myPythonPackages.shirah-reader
-      # myPythonPackages.adafruit-nrfutil
-      # ngrok
-      nix-init
-      niv
-      nodePackages.node2nix
-      nodePackages.pnpm
-      nodePackages.quicktype
-      nodePackages.reveal-md
-      nodePackages.typescript
-      nodejs
-      notify-desktop
-      notify
-      nushell
-      openssl
-      openvpn
-      # openvpn_aws
-      pandoc
-      texlive.combined.scheme-medium
-      typioca
-      php
-      # poetry
-      postgresql
-      (python3.withPackages (
-        ps: with ps; [
-          pyusb
-          tkinter
-        ]
-      ))
-      pwgen
-      # python39Packages.grip
-      # python39Packages.pip
-      # python39Packages.pynvim
-      # python39Packages.setuptools
-      rclone
-      redis
-      renameutils
-      # rustc
-      # rustup
-      sd
-      # steel
-      stern
-      skopeo
-      ssh-to-age
-      tesseract4
-      tio
-      tldr
-      # turbo
-      ttyper
-      # terminal-typeracer
-      unrar
-      wireshark-cli
-      typespeed
-      vegeta
-      # visidata #broken
-      wasmtime
-      yarn
-      yarn2nix
-      yt-dlp
-      yq
-      # yubikey-manager #broken
-      zgrviewer
-      zoxide
-    ];
-  gui =
-    with pkgs;
-    extra
-    ++ [
-      # audacity
-      google-chrome
-      discord
-      emoji-menu
-      power-menu
-      feh
-      inkscape
-      ghostty
-      gimp
-      kitty
-      mlt
-      # memento
-      obsidian
-      openscad
-      qalculate-gtk
-      slack
-      xclip
-      xorg.xmodmap
-      zathura
-      zoom-us
-      plex-mpv-shim
-      anki
-      arandr
-      brightnessctl
-      blender
-      # calibre # Test /get ... /nix/store/ncv68hjnidcd2bm5abkhklrijhn0cgn6-stdenv-linux/setup: line 1721: 20786 Segmentation fault      (core dumped) python setup.py test ${EXCLUDED_FLAGS[@]}
-      cheese
-      dbeaver-bin
-      drawio
-      droidcam
-      easyeda2kicad
-      easyeffects
-      gparted
-      gramps
-      f3d
-      firefox
-      freecad-wayland
-      fstl
-      hakuneko
-      helvum
-      keyd
-      kdePackages.kdenlive
-      kooha
-      krita
-      # kicad-unstable
-      libreoffice
-      mpv
-      neovide
-      # obs-studio # sudo modprobe v4l2loopback devices=1 video_nr=21 card_label="OBS Cam" exclusive_caps=1
-      v4l-utils
-      # plover-dev
-      pulsemixer
-      # plexamp
-      # postman not found error
-      remote-touchpad
-      screenkey
-      # orca-slicer
-      # super-slicer-latest
-      prusa-slicer
-      telegram-desktop
-      via
-      vial
-      vlc
-      warpd
-      wasistlos # whatsapp-for-linux
-      dragon-drop
-      yacreader
-    ];
-  gaming = with pkgs; [
-    alvr
+  extra = default ++ [
+    # fusee-launcher
+    # kdePackages.k3b # export QTWEBENGINE_DISABLE_SANDBOX=1; sudo -EH k3b
+    # mimic
+    # musikcube
+    # myPythonPackages.adafruit-nrfutil
+    # myPythonPackages.shirah-reader
+    # ngrok
+    # nix-du
+    # openvpn_aws
+    # poetry
+    # python311Packages.adafruit-nrfutil
+    # python39Packages.grip
+    # python39Packages.pip
+    # python39Packages.pynvim
+    # python39Packages.setuptools
+    # rustc
+    # rustup
+    # steel
+    # terminal-typeracer
+    # turbo
+    # visidata #broken
+    # yubikey-manager #broken
+    (lua.withPackages (ps: with ps; [ luacheck ]))
+    (python3.withPackages (
+      ps: with ps; [
+        pyusb
+        tkinter
+      ]
+    ))
+    acpi
+    age
+    air
+    ansifilter
+    appimage-run
+    archivemount
+    autoreconfHook
+    avrdude
+    binutils
+    blisp
+    btop
+    btop
+    bun
+    cargo
+    cargo-wasi
+    cheat
+    clang-tools
+    delve
+    deno
+    difftastic
+    dive
+    docker-buildx
+    docker-compose
+    eksctl
+    evtest
+    exiv2
+    ffmpeg
+    file
+    fluxcd
+    ghc
+    glow
+    gnupg
+    gnuplot
+    go
+    gotypist
+    graphviz
+    hdparm
+    hexdino
+    hottext
+    imagemagick
+    iotop
+    iputils
+    jc
+    jdk11
+    joshuto
+    k9s
+    kaf
+    kanata
+    kind
+    kmonad
+    krename
+    kubectl
+    kubectx
+    kubernetes-helm
+    kubetail
+    lldb
+    lsof
+    manga-tui
+    massren
+    mdbook
+    mdl
+    mecab
+    mkvtoolnix-cli
+    mysql80
+    niv
+    nix-index
+    nix-init
+    nodePackages.node2nix
+    nodePackages.pnpm
+    nodePackages.quicktype
+    nodePackages.reveal-md
+    nodePackages.typescript
+    nodejs
+    notify
+    notify-desktop
+    ns-usbloader
+    nushell
+    openssl
+    openvpn
+    pandoc
+    patchelf
+    php
+    pinentry-curses
+    postgresql
+    pwgen
+    qmk
+    rclone
+    redis
+    renameutils
+    sd
+    skopeo
+    speedread
+    ssh-to-age
+    stable.awscli2
+    stern
+    strace
+    tagainijisho
+    tesseract4
+    texlive.combined.scheme-medium
+    tio
+    tiramisu
+    tldr
+    traceroute
+    ttyper
+    typespeed
+    typioca
+    unrar
+    usbimager
+    usbutils
+    vegeta
+    ventoy
+    wasmtime
+    wireshark-cli
+    xdotool
+    xorg.xhost # add root to xsession: xhost si:localuser:root
+    yarn
+    yarn2nix
+    yq
+    yt-dlp
+    zgrviewer
+    zoxide
+  ];
+  gui = extra ++ [
+    # audacity
+    # calibre # Test /get ... /nix/store/ncv68hjnidcd2bm5abkhklrijhn0cgn6-stdenv-linux/setup: line 1721: 20786 Segmentation fault      (core dumped) python setup.py test ${EXCLUDED_FLAGS[@]}
+    # kicad-unstable
+    # memento
+    # obs-studio # sudo modprobe v4l2loopback devices=1 video_nr=21 card_label="OBS Cam" exclusive_caps=1
+    # orca-slicer
+    # plexamp
+    # plover-dev
+    # postman not found error
+    # super-slicer-latest
+    anki
+    arandr
+    blender
+    brightnessctl
+    cheese
+    dbeaver-bin
+    discord
+    dragon-drop
+    drawio
+    droidcam
+    easyeda2kicad
+    easyeffects
+    emoji-menu
+    f3d
+    feh
+    firefox
+    freecad-wayland
+    fstl
+    ghostty
+    gimp
+    google-chrome
+    gparted
+    gramps
+    hakuneko
+    helvum
+    inkscape
+    kdePackages.kdenlive
+    keyd
+    kitty
+    kooha
+    krita
+    libreoffice
+    mlt
+    mpv
+    neovide
+    obsidian
+    openscad
+    plex-mpv-shim
+    power-menu
+    prusa-slicer
+    pulsemixer
+    qalculate-gtk
+    remote-touchpad
+    screenkey
+    slack
+    telegram-desktop
+    v4l-utils
+    via
+    vial
+    vlc
+    warpd
+    wasistlos # whatsapp-for-linux
+    xclip
+    xorg.xmodmap
+    yacreader
+    zathura
+    zoom-us
+  ];
+  gaming = [
     # chiaki-ng
+    # gamescope
     # minecraft
+    # sidequest
+    # wine64
+    (stable.emulationstation-de { retroarch = retroarchWithCores; })
+    alvr
     bottles
     cdecrypt
     cemu
+    gamemode
     heroic
     igir
     lutris
@@ -317,19 +317,14 @@ let
     mangohud
     maxcso
     moonlight-qt
-    gamemode
-    # gamescope
     protontricks
     protonup-ng
-    # sidequest
+    retool
     retroarchWithCores
     ryubing
     skyscraper
-    (stable.emulationstation-de { retroarch = retroarchWithCores; })
-    retool
     vulkan-tools
     wine
-    # wine64
     winetricks
     xemu
     xenia-canary
