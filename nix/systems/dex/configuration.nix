a20362dc413996a891ec00bdc533e9f6aeea78a3
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-13 15:39:04 +1100

    add steam

--- nix/systems/dex/configuration.nix
+++ nix/systems/dex/configuration.nix
@@ -177,6 +177,14 @@ rec {
 
   environment.systemPackages = pkgs.groups.gui ++ pkgs.groups.gaming;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+  hardware.steam-hardware.enable = true;
+
   services.sunshine = {
     enable = true;
     autoStart = true;
