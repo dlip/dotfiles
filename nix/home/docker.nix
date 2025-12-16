1ea32e9828c9e4185c07bc3153852e65878b3871
Dane Lipscombe <danelipscombe@gmail.com>
2025-12-16 09:40:57 +0000

    Fix rgb

--- nix/home/docker.nix
+++ nix/home/docker.nix
@@ -1,10 +1,22 @@
 { pkgs, ... }:
 {
   home = {
-    packages = pkgs.groups.default;
+    packages =
+      with pkgs;
+      pkgs.groups.default
+      ++ [
+        ncurses
+      ];
     username = "root";
     homeDirectory = "/root";
     stateVersion = "26.05";
+    sessionVariables = {
+      TERMINFO_DIRS = "${pkgs.ncurses}/share/terminfo:${pkgs.kitty}/lib/kitty/terminfo:$HOME/.nix-profile/share/terminfo:/usr/share/terminfo";
+      TERM = "kitty";
+      SHELL = "${pkgs.zsh}/bin/zsh";
+      LANG = "C.UTF-8";
+      LC_ALL = "C.UTF-8";
+    };
   };
-programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
