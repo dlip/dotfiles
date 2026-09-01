6819446b2e24571b753b8fcf79d9524db9b5dbbf
Dane Lipscombe <danelipscombe@gmail.com>
2026-09-01 15:37:56 +1000

    yank

--- .config/nvim/lua/plugins/astrocore.lua
+++ .config/nvim/lua/plugins/astrocore.lua
@@ -1,6 +1,19 @@
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
 
+local function yank_path(modifier, with_line)
+  return function()
+    local path = vim.fn.expand("%" .. modifier)
+    if path == "" then
+      vim.notify("Buffer has no file name", vim.log.levels.WARN)
+      return
+    end
+    if with_line then path = path .. ":" .. vim.fn.line "." end
+    vim.fn.setreg("+", path)
+    vim.notify("Yanked " .. path)
+  end
+end
+
 ---@type LazySpec
 return {
   "AstroNvim/astrocore",
@@ -55,6 +68,21 @@ return {
         },
 
         ["<Leader>a"] = { function() require("snacks").picker.smart() end, desc = "Find files (smart)" },
+        ["<Leader>gr"] = { function() require("snacks").picker.git_grep() end, desc = "Git grep" },
+
+        ["<Leader>y"] = { desc = "Yank path" },
+        ["<Leader>yf"] = { yank_path(":.", false), desc = "Relative filename" },
+        ["<Leader>yF"] = { yank_path(":.", true), desc = "Relative filename with line" },
+        ["<Leader>yp"] = { yank_path(":p", false), desc = "Full path" },
+        ["<Leader>yP"] = { yank_path(":p", true), desc = "Full path with line" },
+
+        -- alternate buffer, quiet when there is no alternate file
+        ["<Tab>"] = {
+          function()
+            if vim.fn.bufnr "#" ~= -1 then vim.cmd.buffer "#" end
+          end,
+          desc = "Alternate buffer",
+        },
       },
     },
   },
